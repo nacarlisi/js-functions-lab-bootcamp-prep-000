cb62e98fd2ae3ee6e2c5f6f7fc186fa68a9d5bb7
Nicholas Carlisi <nacarlisi@gmail.com>
2018-10-18 03:46:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,3 +9,7 @@ function happyHolidaysTo (name) {
 function happyHolidaysTo (holiday, name) {
   return `Happy ${holiday}, ${name}!`
 }
+
+function holidayCountdown(holiday, days) {
+  return `It's ${days}
+}
